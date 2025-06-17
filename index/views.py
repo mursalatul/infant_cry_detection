0f5ab75df68e07884b9573051a999dc5caa4c293
mursalatul <mursalatulislampallob@gmail.com>
2025-06-18 00:33:19 +0600

    added faq section in index page.

--- index/views.py
+++ index/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from result.models import TrustCounter
-
+from index.models import FAQ
 # Create your views here.
 
 def index(request):
     # trust counter
     trust_count, created = TrustCounter.objects.get_or_create(id=1)
     
-    return render(request, 'index.html', {'trust_count': trust_count.count_number,})
+    # faq question & answers
+    faq_data = FAQ.objects.all()
+    print(faq_data)
+    print("ok")
+    return render(request, 'index.html', {'trust_count': trust_count.count_number, 'faq_data': faq_data,})
 
